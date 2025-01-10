1180b1b5453df5dcd066bc22238cfa84ca4269af
d0v0h <00_kdh@naver.com>
2025-01-10 18:04:52 +0000

    Added: router

--- routers/router.py
+++ routers/router.py
@@ -4,6 +4,8 @@ from service.cleanBot.main import CleanBotService
 from service.dependencies import get_clean_bot_service
 from service.dependencies import get_summarizer_service
 from service.summaryText.main import SummarizerService
+from service.dependencies import get_emb_search_service
+from service.embSearch.pydantic_embsearch import VectorizeService, Query
 
 router = APIRouter()
 
@@ -19,3 +21,11 @@ async def summary_notice(text: str, service: SummarizerService = Depends(get_sum
     requested_text = service.create_summary_request(text)
     result = service.summarize_notice(requested_text)
     return result
+
+@router.post("/embSearch")
+async def emb_search(keyword: str, service: VectorizeService = Depends(get_emb_search_service)):
+
+    # 사용자 검색어 처리
+    query = Query(text=keyword)
+    query_vectors = query.to_vector(service)
+    return query_vectors.tolist()
--- service/dependencies.py
+++ service/dependencies.py
@@ -1,8 +1,12 @@
 from .cleanBot.main import CleanBotService
 from .summaryText.main import SummarizerService
+from .embSearch.pydantic_embsearch import VectorizeService
 
 def get_clean_bot_service() -> CleanBotService:
     return CleanBotService()
 
 def get_summarizer_service() -> SummarizerService:
     return SummarizerService()
+
+def get_emb_search_service() -> VectorizeService:
+    return VectorizeService()
